36b7d2caae3b9d8a899a6558764ad11d4afaca03
simizu <94036084+simizuzu@users.noreply.github.com>
2023-10-01 02:44:35 +0900

    02_03終了

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
-	printf("Hello,World!\n");
+int main(int argc, char* argv[]) {
+	//argc‚Ì”‚¾‚¯ŒJ‚èo‚·
+	for (int i = 0; i < argc; i++)
+	{
+		printf(argv[i]);
+		printf("\n");
+	}
 	system("pause");
 
 	return 0;
